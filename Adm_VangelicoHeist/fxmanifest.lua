4e5d20591e509a256862990f332ecb11048742af
Adaamek903 <143213504+Adaamek903@users.noreply.github.com>
2023-08-25 19:46:49 +0200

    Add files via upload

---
+++ Adm_VangelicoHeist/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+name         'tofjewrobbery'
+version      '1.0.1'
+description  'Jewels Robbery system'
+author       'Adaamek'
+
+dependency 'ox_lib'
+
+shared_scripts {
+	'@es_extended/imports.lua',
+	'@ox_lib/init.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+files{
+    'locales/*.json'
+}
+
